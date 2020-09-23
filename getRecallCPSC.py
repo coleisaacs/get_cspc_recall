10d0245ed570660b9702fc652f84f5d8f638beec
Cole Isaacs <coleisaacs@gmail.com>
2020-09-22 22:40:05 -0400

    initial commit

--- getRecallCPSC.py
+++ getRecallCPSC.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env pyython
 # -*- coding: utf-8 -*-
 
 """
@@ -8,25 +8,27 @@ __description__  = "Pulls recall information from the Consumer Product
 __author__ = "Cole Isaacs"
 __credits__ = "Cole Isaacs"
 __license__ = "GNU GPLv3 except dependencies where their licenses apply."
-__dependencies__ = "CSCPC API, Python packages: python-docx, requests."
+__dependencies__ = "CSCPC API, Python packages: python-docx, requests, imageio."
 __version__ = "1.0.1"
 __maintainer__ = "Cole Isaacs"
 __email__ = "coleisaacs@gmail.com"
 __status__ = "Production"
 __notes__  = "CSCPC API: https://cpsc.gov/Recalls/
                          CPSC-Recalls-Application-Program-Interface-API-Information
+             
+             Produced .docx requires some editing. 
              "
 
 """
 
-# Import dependencies. 
+# Import dependencies.
 
 import json
 import requests
+import imageio
 from datetime import date
 from docx import Document
 from docx.shared import Inches
-
 
 # Get current date and rewind one year for starting timeframe of recall list.
 
@@ -35,25 +37,36 @@ year = getCurDate.year - 1
 month = getCurDate.month
 day =  getCurDate.day
 
-date = str(year) + "-" + str(month) + "-" + str(day)
+prepDate = str(year) + "-" + str(month) + "-" + str(day)
 
 # CSCPC API URI parameters.
+def make_call():
+    """
+    Make API call. 
 
-apiCSCPC = "http://www.saferproducts.gov/RestWebServices/Recall?"
-apiParam = "format=json&RecallDateStart=" + str(date)
-apiCall = apiCSCPC + apiParam
+    """
+    global makeCall
+    apiCSCPC = "http://www.saferproducts.gov/RestWebServices/Recall?"
+    apiParam = "format=json&RecallDateStart=" + str(finDate)
+    apiCall = apiCSCPC + apiParam
+    
+    makeCall = requests.get(apiCall).json()
+
+    return makeCall
+
 
 #  Filename to save to for API output
+saveOut = 'recall_' + str(getCurDate) + '.docx'
 
-saveOut = 'recall_' + str(date) + '.docx'
-
-def getDate(date):
+def get_date(prepDate):
     """
     Check date and if not OK, prompt to correct.
 
     """
+    global finDate     
+    
     while True:
-        print("Use " + date + " as the date?")
+        print("Use " + prepDate + " as the date?")
         ans = input("Enter Y or y for Yes and N or n for No: ")
 
 
@@ -61,17 +74,18 @@ def getDate(date):
            ('Y' == ans) or \
            ('n' == ans) or \
            ('N' == ans):
-               
+
            # If valid no action.
+           finDate = prepDate
+           print("OK. ")
            
-            print("OK. ")
-            break
-        
-   
+           break
+
+
     if ("n" == ans) or ("N" == ans):
-        
+
     # Validate user entry.
-    
+
             while True:
                 year = input("Enter year (4-digit/YYYY): ")
                 month = input("Enter month (2-digit/MM): ")
@@ -81,7 +95,7 @@ def getDate(date):
                     year = int(year)
                     month = int(month)
                     day = int(day)
-                    
+
 
                 except:
                     print(
@@ -97,80 +111,114 @@ def getDate(date):
                    (getCurDate.year < year):
 
                     # If out of range restart loop
-                    
+
                     print("Invalid entry.")
                     continue
 
                 else:
-                    
-                    # Set date to string and format for API call.
-                    year = str(year)
-                    month = str(month)
-                    day = str(day)
 
-                if(2 == len(month)) and (2 == len(day)):
-                    date = year + "-" + month + "-" + day
+                     if(2 == len(str(month))) and (2 == len(str(day))):
+                          # Set date to string and format for API call.
+                          year = str(year)
+                          month = str(month)
+                          day = str(day)
 
-                else:
-                    date = year + "-0" + month + "-0" + day
-                
+                          finDate = year + "-" + month + "-" + day
+                        
+                     else:
+                         # Set date to string and format for API call.
+                         year = str(year)
+                         month = str(month)
+                         day = str(day)
+
+                         finDate = year + "-0" + month + "-0" + day
                 break
 
+            return finDate
 
-    return date
+def create_docx():
+    """
+    Parse JSON response and output to a docx.
+
+    """
+
+    print("Creating .docx from: " + str(finDate) + ". Please wait...")
+
+    # Get fresh call. 
+    make_call()
+
+    recs = len(makeCall)
+    rn = 1
+
+    docx = Document()
+   
+
+    
+
+    print("Total Recalls: " + str(recs))
+    print("")
+
+    for i in makeCall:
+        # Display that script is still processing.
+
+        print("Processing: " + str(rn) + "/" + \
+              str(recs) + "  ======== " + \
+              str(i["Products"][0]["Name"]))
+
+        # Create docx.
+
+        p = docx.add_paragraph("")
+        p.add_run(i["Products"][0]["Name"]).bold = True
+
+        p = docx.add_paragraph("")
+        p.add_run("Hazard: ").bold = True
+        p.add_run(i["Hazards"][0]["Name"])
+
+        p = docx.add_paragraph("")
+        p = docx.add_paragraph(i["Description"])
+
+        try:
+            p = docx.add_paragraph(i["Retailers"][0]["Name"])
             
+        except:
+            p = docx.add_paragraph("")
 
-# Confirm starting date for recall timeframe.
-date = getDate(date)
+        p = docx.add_paragraph(i["ConsumerContact"])
 
+        p = docx.add_paragraph("")
+        p.add_run("Remedy: ").bold = True
 
-# TODO: Parse JSON
-
-req =  https://www.saferproducts.gov/RestWebServices/Recall?format=json&RecallDateStart=2020-09-11
-
-request = requests.get(URI).json()
-request[0]['RecallID']
-
-# Get timeframe, default to 11/1/(current year - 1) - 10/31/(current year)
-# http://www.saferproducts.gov/RestWebServices/Recall?format=json&RecallDateStart=2019-11-01
-
-"""
-Keys:
-json[#]["Products"][#]["Name"]
-json[#]["Hazards"][#]["Name"]
-
-json[#]["Description"]                              #Tables Regex?
-json[#]["Retailers"][#]["Name"]
-
-json[#]["Remedies"][#]["Name"]
-json[#]["ConsumerContact"]
-
-json[#]["Images"][#++]["URL"]
+        try:
+            p.add_run(i["Remedies"][0]["Name"])
+            
+        except:
+            p = docx.add_paragraph("")
 
 
+        # Iterate through image URLs and get content.
 
-request[0]["Products"][0]["Name"]
-request[0]["Hazards"][0]["Name"]
-request[0]["Remedies"][0]["Name"]
-request[0]["Description"]
-request[0]["Retailers"][0]["Name"]
-request[0]["ConsumerContact"]
-request[0]["Images"][0]["URL"]
+        for x in i["Images"]:
+            
+            # Check for errors with URL
+            try:
+                imge = imageio.imread(x["URL"])
+                imageio.imwrite('imge.png', imge)
+            
+            except: 
+                p = docx.add_paragraph(x["URL"])
 
+            else:
+                p = docx.add_picture('imge.png')
+        
+        rn += 1
 
-"""
+    docx.save(saveOut)
 
+print("Get Recalls from CPSC.gov")
+print("========================")
 
-# TODO:  .docx formatting
+get_date(prepDate)
 
-document = Document()
+create_docx()
 
-p = document.add_paragraph('Name of Product')
-p = document.add_paragraph('Hazard:')
-p = document.add_paragraph('Hazard Details')
-p = document.add_paragraph('Description'))
-document.add_picture('monty-truth.png', width=Inches(1))
-
-document.save(saveOut)
-
-
+print("========================")
